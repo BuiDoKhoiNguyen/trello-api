64030b4993d4544c282d8281312b6fa478a40072
BuiDoKhoiNguyen <khoinguyen05122004@gmail.com>
2024-03-30 13:26:40 +0700

    fix whitelist domain

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 export const WHITELIST_DOMAINS = [
-    'https://trello-pf38rt7kb-nguyens-projects-88d1b24e.vercel.app/'
+    'https://trello-pf38rt7kb-nguyens-projects-88d1b24e.vercel.app'
 ]
 
 export const BOARD_TYPES = {
