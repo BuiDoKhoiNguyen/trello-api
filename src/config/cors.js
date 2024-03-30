71e7247db115b0318326008b07c691d0f98eb7a3
BuiDoKhoiNguyen <khoinguyen05122004@gmail.com>
2024-03-30 13:36:34 +0700

    cors fix

--- src/config/cors.js
+++ src/config/cors.js
@@ -10,9 +10,11 @@ export const corsOptions = {
     // Allow calling the API using POSTMAN in the dev environment,
     // Usually, when using postman, the origin value will be undefined
     // Update: In video number 75 of the MERN Stack PRO series, when deploying the project to a production server, we will adjust this section a bit more to fit each production or dev environment. Trust me, when learning with me, you can be confident in the thoroughness and accuracy of the instructions :D
-    if (!origin && env.BUILD_MODE === 'dev') {
+    if (env.BUILD_MODE === 'dev') {
       return callback(null, true)
     }
+
+    
 
     // Check if the origin is an accepted domain
     if (WHITELIST_DOMAINS.includes(origin)) {
