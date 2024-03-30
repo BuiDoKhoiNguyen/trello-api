793cabd5b5c63d0639599abcd78082e608f70e25
BuiDoKhoiNguyen <khoinguyen05122004@gmail.com>
2024-03-30 13:18:23 +0700

    white list domain

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,7 @@
+export const WHITELIST_DOMAINS = [
+    'https://trello-pf38rt7kb-nguyens-projects-88d1b24e.vercel.app/'
+]
+
 export const BOARD_TYPES = {
     PUBLIC: 'public',
     PRIVATE: 'private'
